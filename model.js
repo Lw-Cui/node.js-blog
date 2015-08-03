15808bc28e40a483438ed678db3ea2666488a669
Lw-Cui <645739460@QQ.com>
2015-08-03 19:34:59 +0800

    finish blog

--- model.js
+++ model.js
@@ -2,6 +2,38 @@
  *
  * Created by lw on 15-8-2.
  */
+exports.Auth = function () {
+    this.sql = require('mysql');
+    this.connection = this.sql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'greatclw',
+        database: 'blog'
+    });
+    this.connection.connect();
+};
+
+
+exports.Auth.prototype.login = function (username, password, set_id) {
+    var str =
+        'SELECT * FROM user WHERE username="'
+        + username + '"and password="' + password + '";';
+    console.log(str);
+    var id = -1;
+    this.connection.query(str, function(error, results, fields) {
+        if (results.length)
+            set_id(results[0].user_id);
+        else
+            set_id(-1);
+    });
+};
+
+exports.Auth.prototype.register = function (username, password) {
+    var str =
+        'INSERT INTO user VALUES(NULL, "' + username + '", "' + password + '");';
+    console.log(str);
+    this.connection.query(str);
+};
 
 
 exports.Blog = function () {
@@ -15,33 +47,38 @@ exports.Blog = function () {
     this.connection.connect();
 };
 
-exports.Blog.prototype.new_post = function (title, content) {
-
+exports.Blog.prototype.new_post = function (title, content, user_id) {
     var str =
-        'INSERT INTO blog VALUES(NULL, "' + title + '", "' + content + '");';
+        'INSERT INTO article VALUES(NULL, "' + title + '", "' + content + '",' + user_id + ');';
     console.log(str);
     this.connection.query(str);
 };
 
-exports.Blog.prototype.query = function (key, value) {
+exports.Blog.prototype.query = function (key, value, show) {
     var str =
-        'SELECT * FROM blog WHERE ' + key + '="' + value + '";';
+        'SELECT * FROM article WHERE ' + key + '="' + value + '";';
     console.log(str);
     this.connection.query(str, function(error, results, fields) {
-        console.log(results);
-    })
+        show(results);
+    });
 };
 
 exports.Blog.prototype.edit = function (id, title, content) {
     var str =
-        'UPDATE blog SET title="' + title + '", content="' + content + '"' + 'WHERE id=' + id + ';';
+        'UPDATE article SET title="' + title + '", content="' + content + '"' + 'WHERE article_id=' + id + ';';
     console.log(str);
     this.connection.query(str);
 };
 
 exports.Blog.prototype.delete = function (id) {
     var str =
-        'DELETE FROM blog WHERE id=' + id + ';'
+        'DELETE FROM article WHERE article_id=' + id + ';';
     console.log(str);
     this.connection.query(str);
+};
+
+exports.Blog.prototype.register = function (username, password) {
+    var str =
+        'INSERT INTO user VALUES(NULL, "' + username + '", "' + pasword + '");';
+
 };
--- routes.js
+++ routes.js
@@ -5,77 +5,88 @@
 var express = require('express');
 var app = express();
 
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 var body_parser = require('body-parser');
 app.use(body_parser());
 
-app.use(express.static('static'));
+var session = require('express-session');
+app.use(session({
+    secret: 'blog system'
+}));
+
 var model = require("./model");
 app.set('view engine', 'jade');
 
 var blog = new model.Blog();
+var auth = new model.Auth();
 
-app.get('/', function (req, res) {
+var checkAuth = function (req, res, next) {
+    if (req.session.user_id) {
+        next();
+    } else {
+        res.send("you have no right to take the operation\n");
+    }
+};
 
-});
-
-app.get('/register', function (req, res) {
-
+app.post('/register', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    auth.register(username, password);
+    req.send(username + " Register successfully\n");
 });
 
 app.post('/login', function (req, res) {
-
+    var username = req.body.username;
+    var password = req.body.password;
+    auth.login(username, password, function (id) {
+        if (id != -1) {
+            req.session.user_id = id;
+            res.send("you are logged in successfully\n");
+        } else {
+            res.send("username doesn't match password\n");
+        }
+    });
 });
 
-app.post('/logout', function (req, res) {
-
-});
-
-app.get('/about', function (req, res) {
-
+app.get('/logout', function (req, res) {
+    delete req.session.user_id;
+    res.send("log out\n");
 });
 
 
-app.post('/add_post', function (req, res) {
+app.post('/add_post', checkAuth, function (req, res) {
     var title = req.body.title;
     var content = req.body.content;
-    res.send('Your title: ' + title + '\nYour content: ' + content);
-    blog.new_post(title, content);
-});
-
-app.get('/add_post', function (req, res) {
-    res.render('add_post', {});
+    blog.new_post(title, content, req.session.user_id);
+    res.send("you are operating as " + req.session.user_id + 'to add post\n');
 });
 
 
-app.post('/edit_post/:id', function (req, res) {
-    blog.query('id', req.param('id'));
+app.post('/edit_post/:id', checkAuth, function (req, res) {
     var title = req.body.title;
     var content = req.body.content;
     blog.edit(Number(req.param('id')), title, content);
-    blog.query('id', req.param('id'));
+    res.send("you are operating as " + req.session.user_id  + 'edit post\n');
 });
 
-app.get('/del_post/:id', function (req, res) {
-    blog.query('id', req.param('id'));
+app.get('/del_post/:id', checkAuth, function (req, res) {
     blog.delete(Number(req.param('id')));
-    blog.query('id', req.param('id'));
+    res.send("you are operating as " + req.session.user_id + 'delete post\n');
 });
 
-app.get('/archives/', function (req, res) {
-});
-
-app.get('/tag/:tag_name/:num', function (req, res) {
-
-});
 
 app.get('/post/:title', function (req, res) {
-    var post = blog.query('title', req.param('title'));
-    res.send(JSON.stringify(post));
+    var post = blog.query('title', req.param('title'), function (post) {
+        res.send(JSON.stringify(post) + '\n');
+    });
 });
 
 app.get('/post_id/:id', function (req, res) {
-    var post = blog.query('id', Number(req.param('id')));
-    res.send(JSON.stringify(post));
+    var post = blog.query('article_id',req.param('id'), function (post) {
+        res.send(JSON.stringify(post) + '\n');
+    });
 });
 
 var port = 3000;
